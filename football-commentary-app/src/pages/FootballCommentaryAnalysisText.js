ac8791c77a807e5ae64de899eb56fb030db801b0
Michael J. Fordham <michael97f@gmail.com>
2023-03-30 11:25:47 +0100

    removed text length check

--- football-commentary-app/src/pages/FootballCommentaryAnalysisText.js
+++ football-commentary-app/src/pages/FootballCommentaryAnalysisText.js
@@ -1,7 +1,5 @@
 import styles from '@/styles/FootballCommentaryAnalysisText.module.css';
 
 export default function FootballCommentaryAnalysisText({ text }) {
-  return (
-    <>{text.length !== 0 && <p className={styles.commentaryText}>{text}</p>}</>
-  );
+  return <p className={styles.commentaryText}>{text}</p>;
 }
